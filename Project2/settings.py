1423489b2a8da1e65fbfad3fd419f7561342dba0
Somali99 <syoyo1095@gmail.com>
2023-12-25 15:51:45 +0100

    First commit after setup on new deb machine

--- Project2/settings.py
+++ Project2/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = [config('ALLOWED_HOST1')]
+ALLOWED_HOSTS = [config('ALLOWED_HOST1'),]
 
 # Application definition
 
